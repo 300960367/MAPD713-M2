7ea90580c8902bf863256a1677aa5593a11c8d42
300960367 <fito@my.centennialcollege.ca>
2017-12-12 23:36:59 -0500

    Database changes to access MongoDB in the Cloud
    url: 'mongodb://300960367:F3rn4nd0@ds129166.mlab.com:29166/map713'

---
+++ database.config.js
@@ -0,0 +1,13 @@
+/* MAPD713 - Enterprise Technologies for Mobile Platforms
+ * Fernando Ito - 10/29/2017
+ * filename...: database.config.js
+ * description: import database configuration
+ * version....: 0.1
+ *
+ * Adapted from: https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
+ */
+
+ module.exports = {
+//   url: 'mongodb://localhost:27017/easy-patients'
+    url: 'mongodb://300960367:F3rn4nd0@ds129166.mlab.com:29166/map713'
+ }
